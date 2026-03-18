abd933b37b0a723e8fa733da4d6c3cd9c64b592e
kolefield <kc@klfld.net>
2026-03-18 18:52:15 -0400

    weeeee

---
+++ skate_better_emotes.lua
@@ -0,0 +1,376 @@
+local defaultPack = {
+    ["xddwalk"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\xddwalk.tga:28:28",
+    ["bla"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\bla.tga:28:28",
+    ["catKiss"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\catKiss.tga:28:28",
+    ["cattime"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\cattime.tga:28:28",
+    ["dankarangry"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\dankarangry.tga:28:28",
+    ["eggman"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\eggman.tga:28:28",
+    ["zappride"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\zappride.tga:28:28",
+    ["bungo"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\bungo.tga:28:28",
+    ["joel"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\joel.tga:28:28",
+    ["breakdance"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\breakdance.tga:28:28",
+    ["catdespair"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\catdespair.tga:28:28",
+    ["caught"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\caught.tga:28:28",
+    ["evaporate"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\evaporate.tga:28:28",
+    ["revaporate"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\revaporate.tga:28:28",
+    [":goyf:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\goyf.tga:28:28",
+    [":ok:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\ok.tga:28:28",
+    ["pedro"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\pedro.tga:28:28",
+    ["cumby"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\cumby.tga:28:28",
+    [":deal:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\deal.tga:28:28",
+    [":nodeal:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\nodeal.tga:28:28",
+    ["cuh"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\cuh.tga:28:28",
+    ["idkwtfthisisbutparsedwantedmetoaddit"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\idkwtfthisisbutparsedwantedmetoaddit.tga:28:28",
+    ["uuh"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\uuh.tga:28:28",
+    ["waaa"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\waaa.tga:28:28",
+    ["jorkin"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\jorkin.tga:28:28",
+    ["turbojorkin"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\turbojorkin.tga:28:28",
+    ["slowjorkin"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\slowjorkin.tga:28:28",
+    ["banan"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\banan.tga:28:28",
+    ["catdespair"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\catdespair.tga:28:28",
+    ["catpoint"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\catpoint.tga:28:28",
+    ["catstare"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\catstare.tga:28:28",
+    [":erm:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\erm.tga:28:28",
+    ["fouge"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\fouge.tga:28:28",
+    [":looking:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\looking.tga:28:28",
+    [":lookup:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\lookup.tga:28:28",
+    ["meandu"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\meandu.tga:28:28",
+    [":pls:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\pls.tga:28:28",
+    ["reallymad"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\reallymad.tga:28:28",
+    [":uhh:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\uhh.tga:28:28",
+    [":wiz:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\wiz.tga:28:28",
+    ["fredpls"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\fredpls.tga:28:28",
+    ["scweam"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\scweam.tga:28:28",
+    ["DIE"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\DIE.tga:28:28",
+    [":HOLY:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\HOLY.tga:28:28",
+    [":ta:"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\ta.tga:28:28",
+    ["freakbomb"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\freakbomb.tga:28:28",
+    ["holymoly"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\holymoly.tga:28:28",
+    ["mhmm"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\mhmm.tga:28:28",
+    ["sadding"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\sadding.tga:28:28",
+    ["nerdle"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\nerdle.tga:28:28",
+    ["tssk"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\tssk.tga:28:28",
+    ["diesofcringe"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\diesofcringe.tga:28:28",
+    ["wiudebuff"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\wiudebuff.tga:28:28",
+    ["damn"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\damn.tga:28:28",
+    ["plead"] = "Interface\\AddOns\\SkateBetterEmotes\\Emotes\\plead.tga:28:28",
+}
+
+local animatedPack = {
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\xddwalk.tga"] = {["nFrames"] = 20, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=32, ["imageHeight"]=1024, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\catKiss.tga"] = {["nFrames"] = 51, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=32, ["imageHeight"]=2048, ["framerate"] = 20}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\cattime.tga"] = {["nFrames"] = 155, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=32, ["imageHeight"]=8192, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\eggman.tga"] = {["nFrames"] = 26, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=32, ["imageHeight"]=1024, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\joel.tga"] = {["nFrames"] = 137, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 8192, ["framerate"] = 60},
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\breakdance.tga"] = {["nFrames"] = 11, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 512, ["framerate"] = 10}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\evaporate.tga"] = {["nFrames"] = 47, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 2048, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\revaporate.tga"] = {["nFrames"] = 50, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 2048, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\pedro.tga"] = {["nFrames"] = 190, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 8192, ["framerate"] = 26}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\idkwtfthisisbutparsedwantedmetoaddit.tga"] = {["nFrames"] = 20, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 1024, ["framerate"] = 20}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\jorkin.tga"] = {["nFrames"] = 5, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 256, ["framerate"] = 15}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\turbojorkin.tga"] = {["nFrames"] = 5, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 256, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\slowjorkin.tga"] = {["nFrames"] = 5, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 256, ["framerate"] = 5}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\fredpls.tga"] = {["nFrames"] = 130, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 8192, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\DIE.tga"] = {["nFrames"] = 40, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 4096, ["framerate"] = 30}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\holymoly.tga"] = {["nFrames"] = 20, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 1024, ["framerate"] = 15}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\nerdle.tga"] = {["nFrames"] = 23, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 1024, ["framerate"] = 10}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\tssk.tga"] = {["nFrames"] = 24, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 1024, ["framerate"] = 24}, -- adjust framerate as needed
+    ["Interface\\AddOns\\SkateBetterEmotes\\Emotes\\diesofcringe.tga"] = {["nFrames"] = 117, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 4096, ["framerate"] = 30}, -- adjust framerate as needed
+}
+
+local emoticons = {
+    ["xddwalk"] = "xddwalk",
+    ["bla"] = "bla",
+    ["catKiss"] = "catKiss",
+    ["cattime"] = "cattime",
+    ["dankarangry"] = "dankarangry",
+    ["eggman"] = "eggman",
+    ["zappride"] = "zappride",
+    ["bungo"] = "bungo",
+    ["joel"] = "joel",
+    ["breakdance"] = "breakdance",
+    ["catdespair"] = "catdespair",
+    ["caught"] = "caught",
+    ["evaporate"] = "evaporate",
+    ["revaporate"] = "revaporate",
+    [":goyf:"] = ":goyf:",
+    [":ok:"] = ":ok:",
+    ["pedro"] = "pedro",
+    ["cumby"] = "cumby",
+    [":deal:"] = ":deal:",
+    [":nodeal:"] = ":nodeal:",
+    ["cuh"] = "cuh",
+    ["idkwtfthisisbutparsedwantedmetoaddit"] = "idkwtfthisisbutparsedwantedmetoaddit",
+    ["uuh"] = "uuh",
+    ["waaa"] = "waaa",
+    ["jorkin"] = "jorkin",
+    ["turbojorkin"] = "turbojorkin",
+    ["slowjorkin"] = "slowjorkin",
+    ["banan"] = "banan",
+    ["catdespair"] = "catdespair",
+    ["catpoint"] = "catpoint",
+    ["catstare"] = "catstare",
+    [":erm:"] = ":erm:",
+    ["fouge"] = "fouge",
+    [":looking:"] = ":looking:",
+    [":lookup:"] = ":lookup:",
+    ["meandu"] = "meandu",
+    [":pls:"] = ":pls:",
+    ["reallymad"] = "reallymad",
+    [":uhh:"] = ":uhh:",
+    [":wiz:"] = ":wiz:",
+    ["fredpls"] = "fredpls",
+    ["scweam"] = "scweam",
+    ["DIE"] = "DIE",
+    [":HOLY:"] = ":HOLY:",
+    [":ta:"] = ":ta:",
+    ["freakbomb"] = "freakbomb",
+    ["holymoly"] = "holymoly",
+    ["mhmm"] = "mhmm",
+    ["sadding"] = "sadding",
+    ["nerdle"] = "nerdle",
+    ["tssk"] = "tssk",
+    ["diesofcringe"] = "diesofcringe",
+    ["wiudebuff"] = "wiudebuff",
+    ["damn"] = "damn",
+    ["plead"] = "plead",
+}
+
+local dropdown = {
+    "SKATE BETTER",
+    "xddwalk",
+    "bla",
+    "catKiss",
+    "cattime",
+    "dankarangry",
+    "eggman",
+    "zappride",
+    "bungo",
+    "joel",
+    "breakdance",
+    "catdespair",
+    "caught",
+    "evaporate",
+    "revaporate",
+--    ":goyf:",
+    ":ok:",
+    "pedro",
+    "cumby",
+    ":deal:",
+    ":nodeal:",
+    "cuh",
+    "idkwtfthisisbutparsedwantedmetoaddit",
+    "uuh",
+    "waaa",
+    "jorkin",
+    "turbojorkin",
+    "slowjorkin",
+    "banan",
+    "catdespair",
+    "catpoint",
+    "catstare",
+    ":erm:",
+    "fouge",
+    ":looking:",
+    ":lookup:",
+    "meandu",
+    ":pls:",
+    "reallymad",
+    ":uhh:",
+    ":wiz:",
+    "fredpls",
+    "scweam",
+    "DIE",
+    ":HOLY:",
+    ":ta:",
+    "freakbomb",
+    "holymoly",
+    "mhmm",
+    "sadding",
+    "nerdle",
+    "tssk",
+    "diesofcringe",
+    "wiudebuff",
+    "damn",
+    "plead",
+}
+
+local function AppendNewEmotes()
+    if not TwitchEmotes_defaultpack then
+        return
+    end
+    if not TwitchEmotes_animation_metadata then
+        return
+    end
+    if not TwitchEmotes_emoticons then
+        return
+    end
+    local function debugPrint(...)
+        debugMode = false -- Change this if you want to debug AppendNewEmotes
+        if debugMode == true then
+            print(...)
+        end
+    end
+
+    for emote, directory in pairs(defaultPack) do
+        TwitchEmotes_defaultpack[emote] = directory
+        debugPrint("DefaultPack - Appended emote:", emote, "Directory:", directory)
+    end
+    for emote, metadata in pairs(animatedPack) do
+        TwitchEmotes_animation_metadata[emote] = metadata
+        debugPrint("AnimatedPack - Appended emote:", emote, "Metadata:", metadata)
+    end
+    for emote, name in pairs(emoticons) do
+        TwitchEmotes_emoticons[emote] = name
+        debugPrint("Emoticons - Appended emote:", emote, "Name:", name)
+    end
+    -- Add custom minimap dropdown
+    TwitchEmotes_dropdown_options[#TwitchEmotes_dropdown_options + 1] = dropdown
+    -- Remove LMAO because i like the text better than the emoji lol
+    TwitchEmotes_defaultpack["LMAO"] = nil
+end
+
+local function escpattern(x)
+    x = string.gsub(x, '%%', '%%%%')
+    x = string.gsub(x, '^%^', '%%^')
+    x = string.gsub(x, '%$$', '%%$')
+    x = string.gsub(x, '%(', '%%(');
+    x = string.gsub(x, '%)', '%%)')
+    x = string.gsub(x, '%.', '%%.')
+    x = string.gsub(x, '%[', '%%[')
+    x = string.gsub(x, '%]', '%%]')
+    x = string.gsub(x, '%*', '%%*')
+    x = string.gsub(x, '%+', '%%+')
+    x = string.gsub(x, '%-', '%%-')
+    x = string.gsub(x, '%?', '%%?')
+    return x
+end
+
+local patterns = {
+    "|TInterface\\AddOns\\TwitchEmotes\\Emotes.-|t",
+    "|TInterface\\AddOns\\SkateBetterEmotes\\Emotes.-|t"
+}
+
+local function TwitchEmotesAnimator_UpdateEmoteInFontString(fontstring, widthOverride, heightOverride)
+    local txt = fontstring:GetText();
+    if (txt ~= nil) then
+        for _, pattern in ipairs(patterns) do
+            for emoteTextureString in string.gmatch(txt, pattern) do
+                local imagepath = string.match(emoteTextureString, "|T(Interface\\AddOns\\TwitchEmotes\\Emotes.-%.tga).-|t")
+                if not imagepath then
+                    imagepath = string.match(emoteTextureString, "|T(Interface\\AddOns\\SkateBetterEmotes\\Emotes.-%.tga).-|t")
+                end
+                if not imagepath then
+                    print('No image path found in SkateBetterEmotes')
+                    return
+                end
+
+                local animdata = TwitchEmotes_animation_metadata[imagepath];
+                -- print(animdata)
+                if (animdata ~= nil) then
+                    local framenum = TwitchEmotes_GetCurrentFrameNum(animdata);
+                    local nTxt;
+                    if(widthOverride ~= nil or heightOverride ~= nil) then
+                        nTxt = string.gsub(txt, escpattern(emoteTextureString),
+                                            TwitchEmotes_BuildEmoteFrameStringWithDimensions(
+                                            imagepath, animdata, framenum, widthOverride, heightOverride))
+                    else
+                        nTxt = string.gsub(txt, escpattern(emoteTextureString),
+                                        TwitchEmotes_BuildEmoteFrameString(
+                                            imagepath, animdata, framenum))
+                    end
+
+                    -- If we're updating a chat message we need to alter the messageInfo as wel
+                    if (fontstring.messageInfo ~= nil) then
+                        fontstring.messageInfo.message = nTxt
+                    end
+                    fontstring:SetText(nTxt);
+                    txt = nTxt;
+                end
+            end
+        end
+    end
+end
+
+local function Emoticons_SetAutoComplete(state)
+    
+    Emoticons_Settings["ENABLE_AUTOCOMPLETE"] = state
+
+    if Emoticons_Settings["ENABLE_AUTOCOMPLETE"] and not autocompleteInited then
+        AllTwitchEmoteNames = {};
+
+        local i = 1;
+        for k, v in pairs(TwitchEmotes_defaultpack) do
+            --Some values in emoticons don't have a corresponding key in TwitchEmotes_defaultpack
+            --we need to filter these out because we don't have an emote to show for these
+            -- if TwitchEmotes_defaultpack[v] ~= nil then
+                local excluded = false;
+                for j=1, #TwitchEmotes_ExcludedSuggestions do
+                    if k == TwitchEmotes_ExcludedSuggestions[j] then
+                        excluded = true;
+                        break;
+                    end
+                end
+
+                if excluded == false then
+                    AllTwitchEmoteNames[i] = k;
+                    i = i + 1;
+                end
+            -- end
+        end
+
+        --Sort the list alphabetically
+        table.sort(AllTwitchEmoteNames)
+
+        for _, frameName in pairs(CHAT_FRAMES) do
+            local frame = _G[frameName]
+
+            local editbox = frame.editBox;
+            local suggestionList = AllTwitchEmoteNames;
+            local maxButtonCount = 20;
+
+            local autocompletesettings = {
+                perWord = true,
+                activationChar = ':',
+                closingChar = ':',
+                minChars = 2,
+                fuzzyMatch = true,
+                onSuggestionApplied = function(suggestion)
+                    UpdateEmoteStats(suggestion, true, false, false);
+                end,
+                renderSuggestionFN = Emoticons_RenderSuggestionFN,
+                suggestionBiasFN = function(suggestion, text)
+                    --Bias the sorting function towards the most autocompleted emotes
+                    if TwitchEmoteStatistics[suggestion] ~= nil then
+                        return TwitchEmoteStatistics[suggestion][1] * 5
+                    end
+                    return 0;
+                end,
+                interceptOnEnterPressed = true,
+                addSpace = true,
+                useTabToConfirm = Emoticons_Settings["AUTOCOMPLETE_CONFIRM_WITH_TAB"],
+                useArrowButtons = true,
+            }
+
+            SetupAutoComplete(editbox, suggestionList, maxButtonCount, autocompletesettings);
+            
+        end
+    
+        autocompleteInited = true;
+    end
+
+end
+
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+
+frame:SetScript("OnEvent", function(self, event, addonName)
+    if addonName == "SkateBetterEmotes" then
+        AppendNewEmotes()
+        _G.TwitchEmotesAnimator_UpdateEmoteInFontString = TwitchEmotesAnimator_UpdateEmoteInFontString
+        _G.Emoticons_SetAutoComplete = Emoticons_SetAutoComplete
+        Emoticons_SetAutoComplete(Emoticons_Settings["ENABLE_AUTOCOMPLETE"])
+        self:UnregisterEvent("ADDON_LOADED")
+    end
+end)
